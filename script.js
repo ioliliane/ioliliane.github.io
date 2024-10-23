5fa28eeb7951bb93e0635719a0ee482eb93bf0ef
mpakhlya <mrpakhlyan@gmail.com>
2024-10-23 20:05:57 +0400

    test new version

--- script.js
+++
@@ -1,4 +0,0 @@
-// Show contact section when "Contact" is clicked
-document.getElementById('contactButton').addEventListener('click', function() {
-    document.getElementById('contact').classList.toggle('hidden');
-});
